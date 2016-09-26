4759e9cbb98a2f01a62e6792ffe6fcd087999e06
Fernandoggs <fernando.ggs@gmail.com>
2016-09-26 17:26:03 +0200

    Pruebo que trocea una linea y saca el operador

--- calcplus.py
+++ calcplus.py
@@ -5,34 +5,30 @@ import csv
 
 with open('fichero.csv')as mifichero:
     datos = csv.reader (mifichero)
+
+
+if __name__ == "__main__":
+
     for linea in datos:
         print(linea)
 
-class CalculadoraHija(calcoo.Calculadora):
-    def multiplicar(self, op1, op2):
-	    return op1 * op2
-
-    def dividir(self, op1, op2):
-        if op2 == 0:
-            sys.exit("Division by zero is not allowed")
-        else:  
-            return op1 / op2
-
-if __name__ == "__main__":
+    for linea in mifichero
+        operador = linea.split(',')[0]
+        print(operador)
     try:
         operando1 = int(sys.argv[1])
         operando2 = int(sys.argv[3])
     except ValueError:
         sys.exit("Error: Non numerical parameters")
 
-    calculadora = CalculadoraHija()
-    if sys.argv[2] == "multiplica":
+    calculadora = calcoohija.CalculadoraHija()
+    if operador == "multiplica":
         result = calculadora.multiplicar(operando1, operando2)
-    elif sys.argv[2] == "divide":
+    elif operador == "divide":
         result = calculadora.dividir(operando1, operando2)
-    elif sys.argv[2] == "suma":
+    elif soperador == "suma":
         result = calculadora.suma(operando1, operando2)
-    elif sys.argv[2] == "resta":
+    elif operador == "resta":
         result = calculadora.resta(operando1, operando2)
     else:
         sys.exit('Operación sólo puede ser suma, resta, multiplica o divide.')
