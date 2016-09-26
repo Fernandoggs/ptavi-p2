4337e925a57260ac1292635a141cb6eb72e0fdb2
Fernandoggs <fernando.ggs@gmail.com>
2016-09-26 19:21:09 +0200

    Prueba funcionamiento correcto

--- calcplus.py
+++ calcplus.py
@@ -1,36 +1,48 @@
 import sys
 import calcoohija
-
 import csv
 
-with open('fichero.csv')as mifichero:
-    datos = csv.reader (mifichero)
+
 
 
 if __name__ == "__main__":
 
-    for linea in datos:
-        print(linea)
+    with open('fichero.csv')as mifichero:
+        datos = csv.reader (mifichero)
+        calculadora = calcoohija.CalculadoraHija()
+        for linea in datos:
+            #print(linea)
 
-    for linea in mifichero
-        operador = linea.split(',')[0]
-        print(operador)
-    try:
-        operando1 = int(sys.argv[1])
-        operando2 = int(sys.argv[3])
-    except ValueError:
-        sys.exit("Error: Non numerical parameters")
+            for palabra in linea:
+                operador = palabra
+                #print(operador)
+                linea.pop(0)
+                if operador == "suma":
+                    result = 0
+                    print("Result suma:")
+                    for numero in linea:
+                        result = calculadora.sumar(result,int(numero))
 
-    calculadora = calcoohija.CalculadoraHija()
-    if operador == "multiplica":
-        result = calculadora.multiplicar(operando1, operando2)
-    elif operador == "divide":
-        result = calculadora.dividir(operando1, operando2)
-    elif soperador == "suma":
-        result = calculadora.suma(operando1, operando2)
-    elif operador == "resta":
-        result = calculadora.resta(operando1, operando2)
-    else:
-        sys.exit('Operación sólo puede ser suma, resta, multiplica o divide.')
+                elif operador == "resta":
+                    result = int(linea[0])
+                    print("Result resta")
+                    linea.pop(0)
+                    for numero in linea:
+                        result = calculadora.restar(result,int(numero))
 
-    print(result)
+                elif operador == "multiplica":
+                    result = int(linea[0])
+                    print("Result multiplica")
+                    linea.pop(0)
+                    for numero in linea:
+                        result = calculadora.multiplicar(result,int(numero))
+
+                elif operador == "divide":
+                    result= int(linea[0])
+                    print("Result divide")
+                    linea.pop(0)
+                    for numero in linea:
+                        result = calculadora.dividir(result,int(numero))
+                        
+
+            print(result)
