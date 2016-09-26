72b96253fb045c86cf1755d07f9ef8a90e782e03
Fernandoggs <fernando.ggs@gmail.com>
2016-09-26 19:33:28 +0200

    Calcplusplus funcionando

---
+++ calcplusplus.py
@@ -0,0 +1,43 @@
+import sys
+import calcoohija
+import csv
+
+
+
+
+if __name__ == "__main__":
+
+    with open('fichero.csv')as mifichero:
+        datos = csv.reader (mifichero)
+        calculadora = calcoohija.CalculadoraHija()
+        for linea in datos:
+            #print(linea)
+
+            for palabra in linea:
+                operador = palabra
+                #print(operador)
+                linea.pop(0)
+                if operador == "suma":
+                    result = 0
+                    for numero in linea:
+                        result = calculadora.sumar(result,int(numero))
+
+                elif operador == "resta":
+                    result = int(linea[0])
+                    linea.pop(0)
+                    for numero in linea:
+                        result = calculadora.restar(result,int(numero))
+
+                elif operador == "multiplica":
+                    result = int(linea[0])
+                    linea.pop(0)
+                    for numero in linea:
+                        result = calculadora.multiplicar(result,int(numero))
+
+                elif operador == "divide":
+                    result= int(linea[0])
+                    linea.pop(0)
+                    for numero in linea:
+                        result = calculadora.dividir(result,int(numero))
+                        
+            print(result)
